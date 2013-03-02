049d0a530d96ea1891270f506dd524624d8be458
alopex <alopex94@gmail.com>
2013-03-02 18:47:00 +0100

    / ‘Biblioteca/src/biblioteca/Libro.java’

--- Biblioteca/src/biblioteca/Libro.java
+++ Biblioteca/src/biblioteca/Libro.java
@@ -55,21 +55,21 @@ public class Libro {
     
     public int ricercaPerAutore(Libro vet[], String daRicercare){
          for (int i=0; i<vet.length; i++)
-          if (vet[i].Autore.equals(daRicercare)==true)
+          if (vet[i].Autore.equalsIgnoreCase(daRicercare)==true)
               return i;
      return -1; //restituisce -1 se non trova l'elemento da cercare
     }
     
     public int ricercaPerTitolo(Libro vet[], String daRicercare){
          for (int i=0; i<vet.length; i++)
-          if (vet[i].Titolo.equals(daRicercare)==true)
+          if (vet[i].Titolo.equalsIgnoreCase(daRicercare)==true)
               return i;
      return -1; //restituisce -1 se non trova l'elemento da cercare
     }
     
     public int ricercaPerCodice(Libro vet[], String daRicercare){
          for (int i=0; i<vet.length; i++)
-          if (vet[i].Codice.equals(daRicercare)==true)
+          if (vet[i].Codice.equalsIgnoreCase(daRicercare)==true)
               return i;
      return -1; //restituisce -1 se non trova l'elemento da cercare
     }
